f09a0726def0a0e62a9d384e60a8e2a94fb47fbd
tedmartn <tedmart92@gmail.com>
2021-03-01 10:57:44 -0600

    commit

--- adobeReaderChoco.ps1
+++
@@ -1,5 +0,0 @@
-if(!(Test-Path "C:\Program Data\Chocolatey")){
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | Out-Null
-}
-
-Start-Process -Wait -FilePath "C:\ProgramData\chocolatey\choco.exe" -ArgumentList "install adobereader.install -y"
